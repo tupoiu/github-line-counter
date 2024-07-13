90164147764c87f35cb83219e07c5f5e687f36c1
tupoiu <54478352+tupoiu@users.noreply.github.com>
2024-07-13 10:57:24 +0100

    Fix bugs

--- count-lines.sh
+++ count-lines.sh
@@ -5,7 +5,7 @@ exit 1
 fi
 
 cd /tmp
-time=$(date +"%Y-%m-%d_%H-%M-%S")
+time=$(date +"%Y-%m-%d_%H-%M-%S_%3N")
 mkdir $time
 cd $time
 git clone "$1" > /dev/null 2>&1
@@ -20,7 +20,7 @@ exit 1
 fi
 
 # Calculate the total number of lines
-total_lines=$(find "$DIR" -type f | grep -v .git | xargs wc -l | awk 'END {print $1}')
+total_lines=$(find "$DIR" -type f | grep -v "/.git" | xargs wc -l | awk 'END {print $1}')
 
 # Print the result
 echo "$total_lines"
