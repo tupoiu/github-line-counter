215576d35126ad77b86aee8da8728f50e66be650
tupoiu <54478352+tupoiu@users.noreply.github.com>
2024-07-30 19:07:41 +0100

    Drop unnecessary comments

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def loc():
         return "", 400
 
     # Count the lines and put the result in a variable
-    # lines = subprocess.(["bash", "count-lines.sh", git_link])
     lines = subprocess.run(["bash", "count-lines.sh", git_link], stdout=subprocess.PIPE, text=True)
 
     if (lines.stderr):
@@ -23,13 +22,3 @@ def loc():
     else: 
         return {"git_link": git_link, "lines": lines.stdout.strip()}, 200
     
-# if __name__ == "__main__":
-#     # This is used when running locally only. When deploying to Google App
-#     # Engine, a webserver process such as Gunicorn will serve the app. This
-#     # can be configured by adding an `entrypoint` to app.yaml.
-#     # Flask's development server will automatically serve static files in
-#     # the "static" directory. See:
-#     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-#     # App Engine itself will serve those files as configured in app.yaml.
-#     app.run(host="127.0.0.1", port=8080, debug=True)
-
